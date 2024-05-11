b00561de07afe36c593b87fe497808a97bf895ae
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 18:01:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = var.project_id
+  region  = "us-central1" # Region is required for some resources
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
