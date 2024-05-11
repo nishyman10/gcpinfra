7fd6733746f3504e6751992ee2c53c47a335414b
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 17:59:10 +0530

    Update and rename infra.tf to variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  default = "fleet-garage-421904"
+}
+
+variable "vm_name" {
+  default = "my-vm"
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
