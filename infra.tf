a567dcb9792f8e6d860cdcaf6b9c1410a2e4218b
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 17:08:12 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -1,8 +1,15 @@
+variable "environment" {
+  default     = "dev"
+  description = "The environment name"
+}
+
 provider "google" {
   project = "fleet-garage-421904"
   region  = "us-central1"
   zone    = "us-central1-a"
 }
+
+data "google_compute_zones" "zones" {}
 
 resource "google_compute_instance" "server" {
   machine_type = "n1-standard-1"
@@ -15,7 +22,7 @@ resource "google_compute_instance" "server" {
     auto_delete = true
   }
   network_interface {
-    subnetwork = google_compute_subnetwork.public-subnetwork.name
+    subnetwork = google_compute_subnetwork.public-subnetwork.name  # Assuming you have a public subnetwork defined
     access_config {}
   }
   can_ip_forward = true
@@ -25,12 +32,28 @@ resource "google_compute_instance" "server" {
     enable-oslogin         = false
     serial-port-enable     = true
   }
-  
- 
+  labels = {
+    git_commit           = "2bdc0871a5f4505be58244029cc6485d45d7bb8e"
+    git_file             = "terraform__gcp__instances_tf"
+    git_last_modified_at = "2022-01-19-17-02-27"
+    git_last_modified_by = "jameswoolfenden"
+    git_modifiers        = "jameswoolfenden__nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "cd562b76-fbce-4e60-8c75-c9f43d7ffc16"
+  }
 }
 
 resource "google_compute_disk" "unencrypted_disk" {
   name = "terragoat-${var.environment}-disk"
-  
-
+  labels = {
+    git_commit           = "2bdc0871a5f4505be58244029cc6485d45d7bb8e"
+    git_file             = "terraform__gcp__instances_tf"
+    git_last_modified_at = "2022-01-19-17-02-27"
+    git_last_modified_by = "jameswoolfenden"
+    git_modifiers        = "jameswoolfenden__nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "bf87b7d7-a9d8-4f03-a0d4-8a4cbb647d03"
+  }
 }
